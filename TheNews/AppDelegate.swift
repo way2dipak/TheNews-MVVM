789f63c08634ddf5b0cb25b9d18f29e5ce7079ce
Nisha <nishapoddar345@gmail.com>
2020-05-02 12:42:29 +0530

    Added Google Sign In

--- TheNews/AppDelegate.swift
+++ TheNews/AppDelegate.swift
@@ -7,16 +7,46 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate{
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GIDSignIn.sharedInstance()?.clientID = "158956149929-mssrkg88kb4j7r93cioi9ivp14roil9h.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance()?.delegate = self
+        
         return true
     }
+}
 
+extension AppDelegate {
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+               return (GIDSignIn.sharedInstance()?.handle(url))!
+       }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error  = error {
+            print(error.localizedDescription)
+            return
+        } else {
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            
+//            let storyboard = UIStoryboard(name: "Dashboard", bundle: nil)
+//            let vc = storyboard.instantiateViewController(withIdentifier: "MainTabBar") as! MainTabBar
+//            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//            appDelegate.window?.rootViewController = vc
+        }
+    }
+    
 }
