a48a4b06d5755ba8bb331ddb48653b562a3fa0b7
Dipak <dipak.singh@embibe.com>
2020-05-02 11:56:13 +0530

    Initial Commit

---
+++ TheNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheNews
+//
+//  Created by DS on 02/05/20.
+//  Copyright © 2020 developer.dipak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
