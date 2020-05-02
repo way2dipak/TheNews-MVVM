5d643884311bd8ecdacef86aa6b624f66d4f51a9
Dipak <dipak.singh@embibe.com>
2020-05-02 12:49:25 +0530

    project setup

---
+++ TheNews/HeadLines/Controller/HeadLinesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HeadLinesViewController.swift
+//  TheNews
+//
+//  Created by DS on 02/05/20.
+//  Copyright © 2020 developer.dipak. All rights reserved.
+//
+
+import UIKit
+
+class HeadLinesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
